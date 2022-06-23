e0b7723512f50f31ec6b9696caa95e2721a17027
jimmy <jimmywu0229@gmail.com>
2022-06-23 21:03:00 +0800

    add new file

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -6,10 +6,21 @@ with open('reviews.txt', 'r') as f:
 
 number = 0
 count = 0
-while count < 100000 :
+while count < len(data)-1 :
   number = number + len(data[count])
   count = count + 1
 print(number/ len(data)) 
 
+good = []
+for g in data:
+	if 'good' in g :
+	  good.append(g)
+print(good[0])
+
+goody = [1 for g in data if 'good ' in g]
+print(goody)
+
 
 print('總共讀取完了共有',len(data),'筆資料')
+
+
